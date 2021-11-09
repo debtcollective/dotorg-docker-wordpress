13199a51c9c6c96bd30c1bc5e7627fb491d87729
Pea <pea@misfist.com>
2021-11-09 15:36:03 -0500

    Update activation script with new theme and plugin path

--- import-db.sh
+++ import-db.sh
@@ -11,5 +11,5 @@ docker exec --user root -w / dotorg-docker-wordpress_db_1 bash -c " mysql -u roo
 docker exec --user root -w / dotorg-docker-wordpress_db_1 bash -c "mysql -u '$DB_NAME' -p'$DB_USER_PASSWORD' '$DB_NAME' -e \"UPDATE wp_options SET option_value='http://localhost:8000' WHERE option_name='siteurl';\""
 docker exec --user root -w / dotorg-docker-wordpress_db_1 bash -c "mysql -u '$DB_NAME' -p'$DB_USER_PASSWORD' '$DB_NAME' -e \"UPDATE wp_options SET option_value='http://localhost:8000' WHERE option_name='home';\""
 # adjust for discrepancy in database and active site functionality theme and dotorg theme
-docker exec --user root -w /var/www/html/ dotorg-docker-wordpress_wordpress_1 bash -c "wp theme activate dotorg-theme"
-docker exec --user root -w /var/www/html/ dotorg-docker-wordpress_wordpress_1 bash -c "wp plugin activate dotorg-site-functionality"
+docker exec --user root -w /var/www/html/ dotorg-docker-wordpress_wordpress_1 bash -c "wp theme activate debtcollective"
+docker exec --user root -w /var/www/html/ dotorg-docker-wordpress_wordpress_1 bash -c "wp plugin activate site-functionality"
