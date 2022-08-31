66de2944a1c8191b36f2f721577c7c821de209a0
Thiago de Mello Bueno <thiago@madeofpeople.org>
2022-08-31 14:41:55 -0400

    changes composer update to composer install

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ cp composer.json wp-app/composer.json
 
 # set github token for composer to install the wp-action-network-events repo
 docker exec --user www-data -w /var/www/html/ dotorg-docker-wordpress_wordpress_1 composer config --auth github-oauth.github.com "$GITHUB_TOKEN"
-docker exec --user www-data -w /var/www/html/ dotorg-docker-wordpress_wordpress_1 composer update
-docker exec --user www-data -w /var/www/html/wp-content/themes/dotorg-theme dotorg-docker-wordpress_wordpress_1 composer update
+docker exec --user www-data -w /var/www/html/ dotorg-docker-wordpress_wordpress_1 composer install
+docker exec --user www-data -w /var/www/html/wp-content/themes/dotorg-theme dotorg-docker-wordpress_wordpress_1 composer install
 # run the db import script to get the latest db
 ./import-db.sh
