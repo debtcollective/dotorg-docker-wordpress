2132fafedd63ba354fc46a7abac89bd1cb83231c
Thiago de Mello Bueno <thiago@madeofpeople.org>
2022-09-01 17:52:01 -0400

    setup.sh now chowns /var/www to www-data

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cp composer.json wp-app/composer.json
 
-docker exec chown -R www-data:www-data /var/www/ 
+docker exec dotorg-docker-wordpress_wordpress_1 chown www-data:www-data /var/www/ 
+
 # set github token for composer to install the wp-action-network-events repo
 docker exec --user www-data -w /var/www/html/ dotorg-docker-wordpress_wordpress_1 composer config --auth github-oauth.github.com "$GITHUB_TOKEN"
 docker exec --user www-data -w /var/www/html/ dotorg-docker-wordpress_wordpress_1 composer install
